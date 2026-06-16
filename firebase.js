6386baa9792eaaf185ccdeedbd6a110f6bca3d55
Money00111 <niyigenaepizo9@gmail.com>
2026-06-16 21:40:12 +0200

    Refactor firebase.js to clean up imports
    Removed redundant imports and analytics initialization.

--- firebase.js
+++ firebase.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
 
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
-
-import {getDatabase }from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
-
-import { getDatabase }from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
-
-const db = getDatabase(app);
-
-export { db };
 const firebaseConfig = {
   apiKey: "AIzaSyDVYmmWJHEiNtQCroj393lLEpgGaW_h0mM",
   authDomain: "e-learning-epi.firebaseapp.com",
@@ -16,12 +8,10 @@ const firebaseConfig = {
   projectId: "e-learning-epi",
   storageBucket: "e-learning-epi.firebasestorage.app",
   messagingSenderId: "814946403235",
-  appId: "1:814946403235:web:257e305f822cf5d3cad016",
-  measurementId: "G-7KKQMNE0JX"
+  appId: "1:814946403235:web:257e305f822cf5d3cad016"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const db = getDatabase(app);
 
