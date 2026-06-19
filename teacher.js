44312dff7f216756f74f73fadfb5bd2e18fbabf1
Money00111 <niyigenaepizo9@gmail.com>
2026-06-19 11:09:14 +0200

    Implement saveLesson function for lesson storage
    Added functionality to save lesson details to Firebase database.

--- teacher.js
+++ teacher.js
@@ -63,7 +63,65 @@ lessons
 alert(
 
 "Lesson Saved"
+import { db } from "./firebase.js";
 
+import {
+ref,
+push
+}
+from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
+
+
+window.saveLesson = function(){
+
+const level =
+document.getElementById("level").value;
+
+const subject =
+document.getElementById("subject").value;
+
+const title =
+document.getElementById("title").value;
+
+const notes =
+document.getElementById("notes").value;
+
+
+const lesson = {
+
+title:title,
+
+notes:notes
+
+};
+
+
+push(
+
+ref(db,
+
+`lessons/${level}/${subject}`)
+
+,
+
+lesson
+
+)
+
+.then(()=>{
+
+alert("✅ Lesson Saved");
+
+})
+
+.catch(err=>{
+
+alert(err.message);
+
+});
+
+
+  }
 );
 
 
