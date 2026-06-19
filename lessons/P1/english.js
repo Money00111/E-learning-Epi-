3d9f329f22e1fa21ce1c7a5483f8141e082ee598
Money00111 <niyigenaepizo9@gmail.com>
2026-06-19 18:49:21 +0200

    Create english.js

---
+++ lessons/P1/english.js
@@ -0,0 +1,82 @@
+const lessons = [
+
+{
+title:"Alphabet",
+
+content:`
+
+<h2>Alphabet</h2>
+
+A B C D E F G
+
+H I J K L M N
+
+O P Q R S T U
+
+V W X Y Z
+
+`
+},
+
+{
+title:"Greetings",
+
+content:`
+
+<h2>Greetings</h2>
+
+Good morning
+
+Good afternoon
+
+Good evening
+
+Good night
+
+`
+},
+
+{
+title:"Family Members",
+
+content:`
+
+<h2>Family Members</h2>
+
+Father
+
+Mother
+
+Brother
+
+Sister
+
+Baby
+
+`
+},
+
+{
+title:"Colours",
+
+content:`
+
+<h2>Colours</h2>
+
+Red
+
+Blue
+
+Green
+
+Yellow
+
+Black
+
+White
+
+`
+
+}
+
+];
