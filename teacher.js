0fd18d0c6edce5c2274d15aed1b4b48df83c3f36
Money00111 <niyigenaepizo9@gmail.com>
2026-06-19 11:15:56 +0200

    Refactor saveLesson to use Firebase database
    Refactored saveLesson function to use Firebase for lesson storage instead of localStorage.

--- teacher.js
+++ teacher.js
@@ -1,75 +1,9 @@
-window.saveLesson=function(){
-
-
-const lesson={
-
-level:
-
-document.getElementById("level").value,
-
-subject:
-
-document.getElementById("subject").value,
-
-title:
-
-document.getElementById("title").value,
-
-content:
-
-document.getElementById("content").value
-
-
-};
-
-
-
-let lessons=
-
-JSON.parse(
-
-localStorage.getItem(
-
-"lessons"
-
-)
-
-)||[];
-
-
-
-lessons.push(
-
-lesson
-
-);
-
-
-
-localStorage.setItem(
-
-"lessons",
-
-JSON.stringify(
-
-lessons
-
-)
-
-);
-
-
-
-alert(
-
-"Lesson Saved"
 import { db } from "./firebase.js";
 
 import {
 ref,
 push
-}
-from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
 
 
 window.saveLesson = function(){
@@ -116,14 +50,8 @@ alert("✅ Lesson Saved");
 
 .catch(err=>{
 
-alert(err.message);
+alert("❌ "+err.message);
 
 });
 
-
-  }
-);
-
-
-
-};
+}
