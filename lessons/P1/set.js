82e0177343d605c58c5fe78e5a26829d4e4300f5
Money00111 <niyigenaepizo9@gmail.com>
2026-06-20 11:21:53 +0200

    Add lessons data for Living Things and Body Parts

---
+++ lessons/P1/set.js
@@ -0,0 +1,45 @@
+const lessons = [
+
+{
+
+title:"Living Things",
+
+content:`
+
+<h2>Living Things</h2>
+
+Human beings
+
+Animals
+
+Plants
+
+`
+
+},
+
+{
+
+title:"Parts of the Body",
+
+content:`
+
+<h2>Parts of the Body</h2>
+
+Head
+
+Eyes
+
+Nose
+
+Mouth
+
+Hands
+
+Legs
+
+`
+
+}
+
+];
