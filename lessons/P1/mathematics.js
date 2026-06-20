e49747f9198edcf83faa2f900be8bb15a584c112
Money00111 <niyigenaepizo9@gmail.com>
2026-06-20 11:43:21 +0200

    Refactor lessons structure in mathematics.js

--- lessons/P1/mathematics.js
+++ lessons/P1/mathematics.js
@@ -1,72 +1,34 @@
-const lessons = [
+window.lessons=[
+
 
 {
+
 title:"Counting Numbers",
 
 content:`
 
 <h2>Counting Numbers</h2>
 
-<p>Numbers from 1 to 100</p>
-
-Examples
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
+<p>1 2 3 4 5 6 7 8 9 10</p>
 
 `
+
 },
 
-{
-title:"Writing Numbers",
-
-content:`
-
-<h2>Writing Numbers</h2>
-
-One
-Two
-Three
-Four
-Five
-
-`
-},
 
 {
+
 title:"Addition",
 
 content:`
 
 <h2>Addition</h2>
 
-2 + 3 = 5
-
-4 + 1 = 5
+2+3=5
 
 `
-},
 
-{
-title:"Subtraction",
-
-content:`
-
-<h2>Subtraction</h2>
-
-5 - 2 = 3
-
-7 - 4 = 3
-
-`
 }
+
 
 ];
