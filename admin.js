2147b65b8955ab1b5a911a95525dbb0ccdc742f6
Money00111 <niyigenaepizo9@gmail.com>
2026-06-20 21:42:24 +0200

    Delete admin.js

--- admin.js
+++
@@ -1,9 +0,0 @@
-<h2>Shyiramo Lesson (Notes)</h2>
-
-<input id="level" placeholder="Primary 1 / S1">
-<input id="subject" placeholder="Mathematics">
-<input id="title" placeholder="Lesson title">
-
-<textarea id="content" placeholder="Andika notes..."></textarea>
-
-<button onclick="saveLesson()">Save Lesson</button>
