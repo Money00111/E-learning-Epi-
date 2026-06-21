bc4cd59b7f39f06e1a3f234a0df76ee1c8586a83
Money00111 <niyigenaepizo9@gmail.com>
2026-06-21 16:16:20 +0200

    Add quizQuestions array with traffic signs questions

---
+++ quiz.js
@@ -0,0 +1,45 @@
+
+window.quizQuestions=[
+
+{
+
+image:"images/traffic.png",
+
+question:"Itara ritukura risobanura iki?",
+
+options:[
+
+"Komeza",
+
+"Hagarara",
+
+"Genda vuba"
+
+],
+
+answer:1
+
+},
+
+{
+
+image:"images/stop.png",
+
+question:"Iki cyapa kivuze iki?",
+
+options:[
+
+"Kwinjira",
+
+"Hagarara",
+
+"Gukomeza"
+
+],
+
+answer:1
+
+}
+
+];
+  
