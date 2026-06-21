828b37214e8fce7f58eb9dea648ac97f1dd19e68
Money00111 <niyigenaepizo9@gmail.com>
2026-06-21 12:39:33 +0200

    Add quiz lessons to quiz.js

--- data/quiz.js
+++ data/quiz.js
@@ -1,1 +1,18 @@
+window.lessons = [
 
+{
+title: "Quiz 1 📝",
+text: "Itara ritukura risobanura iki? A) Komeza B) Hagarara C) Vuba"
+},
+
+{
+title: "Quiz 2 📝",
+text: "STOP sign isobanura iki?"
+},
+
+{
+title: "Quiz 3 📝",
+text: "Ni iki kigomba kubanza kurebwa mbere yo kugenda?"
+}
+
+];
