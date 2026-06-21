bdedd1a29230f9b5db1df0a6ccfee44e5cac74c4
Money00111 <niyigenaepizo9@gmail.com>
2026-06-21 12:21:24 +0200

    Add warning messages for road safety

--- data/warnings.js
+++ data/warnings.js
@@ -1,1 +1,18 @@
+window.lessons = [
 
+{
+title: "Umuhanda Uramanuka ⚠️",
+text: "Gabanya umuvuduko kandi witondere cyane."
+},
+
+{
+title: "Ikorosi Rikabije ⚠️",
+text: "Hindura umuvuduko mbere yo kugera ku ikorosi."
+},
+
+{
+title: "Umuhanda Unyerera ⚠️",
+text: "Twara witonze kuko ushobora kunyerera."
+}
+
+];
