a265848e3029a2d7f7a3c8189294f56f251775f1
Money00111 <niyigenaepizo9@gmail.com>
2026-06-21 12:30:47 +0200

    Add driving lessons to driving.js

--- data/driving.js
+++ data/driving.js
@@ -1,1 +1,18 @@
+window.lessons = [
 
+{
+title: "Uko utwara neza 🚗",
+text: "Banza urebe imbere, inyuma no ku mpande mbere yo kugenda."
+},
+
+{
+title: "Kwirinda impanuka 🚦",
+text: "Wubahirize ibyapa byose n'amatara yo mu muhanda."
+},
+
+{
+title: "Intera hagati y'imodoka 🚘",
+text: "Hagarara kure y'indi modoka kugirango wirinde impanuka."
+}
+
+];
