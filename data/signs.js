dab4801ecf6b9497c9d43d7de6bf8d8ea33fd3f2
Money00111 <niyigenaepizo9@gmail.com>
2026-06-21 11:54:00 +0200

    Refactor traffic signs data structure

--- data/signs.js
+++ data/signs.js
@@ -1,18 +1,10 @@
-const questions = [
-
+window.lessons = [
 {
 title: "STOP",
-text: "Iki cyapa gisaba umushoferi guhagarara mbere yo gukomeza."
+text: "Hagarara mbere yo gukomeza."
 },
-
 {
 title: "No Entry",
-text: "Iki cyapa kibuza ibinyabiziga kwinjira muri uwo muhanda."
-},
-
-{
-title: "Speed Limit 40",
-text: "Ntabwo wemerewe kurenza kilometero 40 ku isaha."
+text: "Ntibyemewe kwinjira."
 }
-
 ];
