dcd68013aa0061e3545f79495e2d20124078c0ec
Money00111 <niyigenaepizo9@gmail.com>
2026-06-24 14:23:45 +0200

    Implement progress bar update in app.js
    Add progress bar functionality that updates every 100ms.

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
+let progress = 0;
 
+
+setInterval(()=>{
+
+if(progress<75){
+
+progress++;
+
+document.getElementById(
+
+"bar"
+
+).style.width=
+
+progress+"%";
+
+
+document.getElementById(
+
+"percent"
+
+).innerText=
+
+progress+"%";
+
+}
+
+
+},100);
+
+
+
+function openPage(page){
+
+localStorage.setItem(
+
+"page",
+
+page
+
+);
+
+
+alert(
+
+page+
+
+" page"
+
+);
+
+  }
