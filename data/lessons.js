595e39ac1e3e1a4f4e65cf4d392ca0b095201b33
Money00111 <niyigenaepizo9@gmail.com>
2026-06-24 20:52:48 +0200

    Add lessons data for traffic signs

---
+++ data/lessons.js
@@ -0,0 +1,22 @@
+window.lessons = [
+{
+image:"images/stop.png",
+title:"STOP SIGN",
+text:"Ugomba guhagarara burundu mbere yo gukomeza.",
+answer:"Hagarara neza mbere yo kugenda."
+},
+
+{
+image:"images/light_red.png",
+title:"RED LIGHT",
+text:"Itara ritukura risobanura guhagarara.",
+answer:"Ntugomba kugenda."
+},
+
+{
+image:"images/give_way.png",
+title:"GIVE WAY",
+text:"Reka abandi babanze.",
+answer:"Ha abandi inzira mbere."
+}
+];
