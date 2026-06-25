cbef1e2aaee9246d2489c3640e29622490289460
Money00111 <niyigenaepizo9@gmail.com>
2026-06-25 20:06:05 +0200

    Add lessons data for driving instructions

--- js/lessons.js
+++ js/lessons.js
@@ -86,7 +86,6 @@ ${l.answer}
 }
 
 
-
 function toggleAnswer(){
 
 const a=document.getElementById("answer");
@@ -176,3 +175,116 @@ document.getElementById(
 p+"%";
 
 }
+
+const lessons = [
+  {
+    title: "Driving Position",
+    image: "images/driving_position.png",
+    text: "Wicara neza ku ntebe, ugafata steering wheel neza."
+  },
+  {
+    title: "Give Way",
+    image: "images/giveway.png",
+    text: "Tanga inzira ku bandi batwaye ibinyabiziga."
+  },
+  {
+    title: "Green Arrow",
+    image: "images/green_arrow.png",
+    text: "Icyatsi kerekana ko wemerewe kunyura."
+  },
+  {
+    title: "Indicator Use",
+    image: "images/indicator_use.png",
+    text: "Koresha indangururamucyo mbere yo guhindura direction."
+  },
+  {
+    title: "Mirror Check",
+    image: "images/mirror_check.png",
+    text: "Reba indorerwamo mbere yo guhindura lane."
+  },
+  {
+    title: "No Entry",
+    image: "images/no_entry.png",
+    text: "Aha ntibyemewe kwinjira."
+  },
+  {
+    title: "No Parking",
+    image: "images/no_parking.png",
+    text: "Ntibyemewe guhagarara aha."
+  },
+  {
+    title: "Pedestrian Crossing",
+    image: "images/pedestrian_crossing.png",
+    text: "Hita witonda aho abanyamaguru banyura."
+  },
+  {
+    title: "Red Pedestrian Light",
+    image: "images/red_pedestrian.png",
+    text: "Aha abanyamaguru bagomba guhagarara."
+  },
+  {
+    title: "Road Works",
+    image: "images/road_works.png",
+    text: "Hari imirimo y'umuhanda, genda buhoro."
+  },
+  {
+    title: "Roundabout",
+    image: "images/roundabout.png",
+    text: "Inzira y’izunguruka, ugomba kuyitaho."
+  },
+  {
+    title: "School Crossing",
+    image: "images/school_crossing.png",
+    text: "Aha abana bambuka, genda gahoro."
+  },
+  {
+    title: "Seat Belt",
+    image: "images/seat_belt.png",
+    text: "Ambara umukandara w’umutekano."
+  },
+  {
+    title: "Slippery Road",
+    image: "images/slippery_road.png",
+    text: "Umuhanda unyerera, genda witonze."
+  },
+  {
+    title: "Speed Limit",
+    image: "images/speed_limit.png",
+    text: "Ntukarenge umuvuduko wagenwe."
+  },
+  {
+    title: "Stop",
+    image: "images/stop.png",
+    text: "Hagarara burundu mbere yo gukomeza."
+  },
+  {
+    title: "Traffic Light (All)",
+    image: "images/traffic_light_all.png",
+    text: "Amatara yose y’ibarabara."
+  },
+  {
+    title: "Traffic Light Green",
+    image: "images/traffic_light_green.png",
+    text: "Icyatsi: genda."
+  },
+  {
+    title: "Traffic Light Red",
+    image: "images/traffic_light_red.png",
+    text: "Umutuku: hagarara."
+  },
+  {
+    title: "Traffic Light Yellow",
+    image: "images/traffic_light_yellow.png",
+    text: "Umuhondo: tegereza."
+  },
+  {
+    title: "Turn Left",
+    image: "images/turn_left.png",
+    text: "Erekana cyangwa hindukira ibumoso."
+  },
+  {
+    title: "Turn Right",
+    image: "images/turn_right.png",
+    text: "Erekana cyangwa hindukira iburyo."
+  }
+];
