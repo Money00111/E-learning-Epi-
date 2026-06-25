433c67bb646edd766df5682b21279a03b958d268
Money00111 <niyigenaepizo9@gmail.com>
2026-06-25 19:23:16 +0200

    Update warnings.js with new warning messages

--- data/warnings.js
+++ data/warnings.js
@@ -1,18 +1,27 @@
-window.lessons = [
+window.lessons=[
 
 {
-title: "Umuhanda Uramanuka ⚠️",
-text: "Gabanya umuvuduko kandi witondere cyane."
+
+image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Slippery_road_warning_sign.svg/512px-Slippery_road_warning_sign.svg.png",
+
+title:"UMUHANDA UNYERERA",
+
+text:"Umuhanda ushobora kunyerera.",
+
+answer:"Gabanya umuvuduko kandi witondere."
+
 },
 
 {
-title: "Ikorosi Rikabije ⚠️",
-text: "Hindura umuvuduko mbere yo kugera ku ikorosi."
-},
 
-{
-title: "Umuhanda Unyerera ⚠️",
-text: "Twara witonze kuko ushobora kunyerera."
+image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Roadworks_sign.svg/512px-Roadworks_sign.svg.png",
+
+title:"ROAD WORKS",
+
+text:"Hari imirimo iri gukorwa.",
+
+answer:"Gabanya umuvuduko kandi ukurikize amabwiriza."
+
 }
 
 ];
