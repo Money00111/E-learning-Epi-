f2957656040b2d14a15b2f75d0c5e465e0eaf8af
Money00111 <niyigenaepizo9@gmail.com>
2026-06-25 19:06:10 +0200

    Update traffic sign images and details

--- data/signs.js
+++ data/signs.js
@@ -1,14 +1,36 @@
 window.lessons = [
+
 {
-image: "images/stop.png",
-title: "STOP SIGN",
-text: "Ugomba guhagarara burundu.",
-answer: "Hagarara mbere yo gukomeza."
+image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Stop_sign_light_red.svg/512px-Stop_sign_light_red.svg.png",
+
+title:"STOP SIGN",
+
+text:"Ugomba guhagarara burundu.",
+
+answer:"Hagarara mbere yo gukomeza."
+
 },
+
 {
-image: "images/giveway.png",
-title: "GIVE WAY",
-text: "Ha abandi inzira.",
-answer: "Reka abandi babanze."
+image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Yield_sign.svg/512px-Yield_sign.svg.png",
+
+title:"GIVE WAY",
+
+text:"Ha abandi inzira.",
+
+answer:"Reka abandi babanze."
+
+},
+
+{
+image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/No_entry_sign.svg/512px-No_entry_sign.svg.png",
+
+title:"NO ENTRY",
+
+text:"Ntibyemewe kwinjira.",
+
+answer:"Ntugomba kwinjira muri uyu muhanda."
+
 }
+
 ];
