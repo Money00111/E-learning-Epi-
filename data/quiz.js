76d21815f0a2f60c49c5c178d748ffaee627f2d6
Money00111 <niyigenaepizo9@gmail.com>
2026-06-25 12:34:24 +0200

    Add quizQuestions data for quiz functionality

---
+++ data/quiz.js
@@ -0,0 +1,46 @@
+window.quizQuestions = [
+
+{
+image:"images/stop.png",
+
+question:"Iki cyapa gisobanura iki?",
+
+options:[
+
+"Gukomeza",
+
+"Hagarara",
+
+"Kwiruka",
+
+"Guhindukira"
+
+],
+
+answer:1
+
+},
+
+{
+
+image:"images/light_red.png",
+
+question:"Itara ritukura risobanura iki?",
+
+options:[
+
+"Hagarara",
+
+"Komeza",
+
+"Gabanya umuvuduko",
+
+"Hindukira"
+
+],
+
+answer:0
+
+}
+
+];
