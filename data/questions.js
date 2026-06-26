a8d4f3fe4ff3f1bad7034a80846ff88e9f41f4e1
Money00111 <niyigenaepizo9@gmail.com>
2026-06-26 09:59:47 +0200

    Update questions.js

--- data/questions.js
+++ data/questions.js
@@ -1,25 +1,133 @@
 const questions = [
 
 {
+  question: "Iyo utwaye imodoka ugomba kwambara iki?",
+  options: ["Inkweto gusa", "Umutwe gusa", "Umukandara w’umutekano", "Ikoti"],
+  answer: 2
+},
+{
+  question: "Iyo uri gutwara ugomba gukora iki mbere yo gutangira?",
+  options: ["Guhita wihuta", "Kureba mirror", "Gusinzira", "Guhamagara"],
+  answer: 1
+},
+{
+  question: "Umutekano wo mu muhanda usaba iki?",
+  options: ["Kunyura vuba", "Kwirinda impanuka", "Guhagarara buri gihe", "Kuvugira kuri telephone"],
+  answer: 1
+},
+{
+  question: "Iyo uri gutwara ntugomba gukora iki?",
+  options: ["Kureba imbere", "Gukoresha telephone", "Kwicara neza", "Gukurikiza amategeko"],
+  answer: 1
+},
+{
+  question: "Umutekano w’abagenzi usaba iki?",
+  options: ["Kwicara neza", "Gufungura urugi igihe cyose", "Gusakuza", "Kwiruka"],
+  answer: 0
+},
 
-image:"images/logo.png",
+{
+  question: "Iyo uri mu muhanda ugomba gukora iki ku bandi batwara?",
+  options: ["Kubaha", "Kubatera ubwoba", "Kubiruka", "Kubafunga inzira"],
+  answer: 0
+},
+{
+  question: "Iyo uri gutwara nijoro ugomba gukora iki?",
+  options: ["Kuzimya amatara", "Gukoresha amatara y’imbere", "Gufunga amaso", "Guhagarara buri gihe"],
+  answer: 1
+},
+{
+  question: "Umuvuduko mwinshi ushobora gutera iki?",
+  options: ["Impano", "Impaka", "Impanuka", "Ibyishimo"],
+  answer: 2
+},
+{
+  question: "Iyo ugiye guhindura inzira ukora iki?",
+  options: ["Guhita uhindura", "Gukoresha indangururamajwi gusa", "Gukoresha signal", "Guhagarara"],
+  answer: 2
+},
+{
+  question: "Iyo uri gutwara unaniwe ukora iki?",
+  options: ["Gukomeza", "Guhagarara kuruhuka", "Kunywa ikawa gusa", "Kwihuta"],
+  answer: 1
+},
 
-question:"Iki cyapa gisobanura iki?",
+{
+  question: "Umutekano wo mu muhanda usaba iki ku modoka yawe?",
+  options: ["Kuyisiga umwanda", "Kuyitaho no kuyigenzura", "Kuyihisha", "Kuyirukanka"],
+  answer: 1
+},
+{
+  question: "Iyo ugiye kwambuka umuhanda ugomba gukora iki?",
+  options: ["Kwiruka", "Kureba impande zombi", "Guhamagara", "Gusakuza"],
+  answer: 1
+},
+{
+  question: "Iyo uri mu modoka ntugomba gukora iki?",
+  options: ["Kwicara neza", "Gukina no kurangara", "Kwambara umukandara", "Kureba imbere"],
+  answer: 1
+},
+{
+  question: "Iyo uri gutwara mu mvura ukora iki?",
+  options: ["Kongera umuvuduko", "Kugabanya umuvuduko", "Gufunga amatara", "Kwiruka"],
+  answer: 1
+},
+{
+  question: "Impamvu yo kwambara umukandara ni iki?",
+  options: ["Kugira stylish", "Kurinda ubuzima", "Kugira imbaraga", "Kugira umutekano wo kurya"],
+  answer: 1
+},
 
-options:[
+{
+  question: "Iyo ubona impanuka ugomba gukora iki?",
+  options: ["Kureba gusa ugakomeza", "Gufasha cyangwa gutabaza", "Guhunga", "Guseka"],
+  answer: 1
+},
+{
+  question: "Iyo uri gutwara ntugomba gukora iki?",
+  options: ["Kunywa inzoga", "Gukurikiza amategeko", "Kureba imbere", "Kwicara neza"],
+  answer: 0
+},
+{
+  question: "Iyo uri gutwara ugomba gukora iki ku mabwiriza?",
+  options: ["Kuyirengagiza", "Kuyubahiriza", "Kuyasetsa", "Kuyihisha"],
+  answer: 1
+},
+{
+  question: "Iyo ugiye guca imbere y’abandi ukora iki?",
+  options: ["Gukoresha signal", "Guhita wihuta", "Gufunga amaso", "Guhagarara"],
+  answer: 0
+},
+{
+  question: "Iyo uri gutwara mu mujyi ukora iki?",
+  options: ["Kwiruka cyane", "Kugabanya umuvuduko", "Guhagarara buri minota", "Kunywa amazi gusa"],
+  answer: 1
+},
 
-"Gukomeza",
-
-"Hagarara",
-
-"Kongera umuvuduko",
-
-"Kunyura"
-
-],
-
-answer:1
-
+{
+  question: "Umutekano wo mu muhanda usaba iki ku muhanda?",
+  options: ["Kuwuhunga", "Kuwubaha", "Kuwusenya", "Kuwiruka"],
+  answer: 1
+},
+{
+  question: "Iyo uri gutwara ugomba gukora iki igihe hari abandi?",
+  options: ["Kubaha inzira", "Kubabuza inzira", "Kubiruka", "Kubatera ubwoba"],
+  answer: 0
+},
+{
+  question: "Iyo uri gutwara ugomba gukora iki ku modoka iri imbere?",
+  options: ["Kuyigonga", "Kuyubahiriza intera", "Kuyirenga vuba", "Kuyihagarika"],
+  answer: 1
+},
+{
+  question: "Iyo uri gutwara ugomba gukora iki ku muryango w’imodoka?",
+  options: ["Kuwufungura uko ubonye", "Kureba neza mbere yo kuwufungura", "Kuwurasa", "Kuwufunga buri gihe"],
+  answer: 1
+},
+{
+  question: "Iyo uri gutwara ukabona abanyamaguru ukora iki?",
+  options: ["Kubaha inzira", "Kubiruka", "Kubabuza", "Kubaseka"],
+  answer: 0
 }
 
 ];
