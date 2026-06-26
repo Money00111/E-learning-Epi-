d18845350015267782087ad2f29954dc8ebf8d4d
Money00111 <niyigenaepizo9@gmail.com>
2026-06-26 09:27:51 +0200

    Add new question with options and answer

--- data/questions.js
+++ data/questions.js
@@ -1,1 +1,25 @@
+const questions = [
 
+{
+
+image:"images/logo.png",
+
+question:"Iki cyapa gisobanura iki?",
+
+options:[
+
+"Gukomeza",
+
+"Hagarara",
+
+"Kongera umuvuduko",
+
+"Kunyura"
+
+],
+
+answer:1
+
+}
+
+];
