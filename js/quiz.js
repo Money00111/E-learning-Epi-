97f0c18cdc2e151296b605d8070e7bd3a03cfadb
Money00111 <niyigenaepizo9@gmail.com>
2026-06-28 11:37:44 +0200

    Refactor quiz.js to use state management

--- js/quiz.js
+++ js/quiz.js
@@ -1,182 +1,67 @@
-let current = 0;
-
-let score = 0;
-
-
+let state = getState();
 
 function loadQuestion(){
 
+  const q = questions[state.current];
 
-const q = questions[current];
+  document.getElementById("question").innerText = q.question;
 
+  document.getElementById("counter").innerText =
+    (state.current + 1) + " / " + questions.length;
 
-document.getElementById(
+  document.getElementById("sign").src = q.image;
 
-"question"
+  const box = document.getElementById("answers");
+  box.innerHTML = "";
 
-).innerText = q.question;
+  q.options.forEach((opt, i) => {
 
+    const btn = document.createElement("button");
+    btn.innerText = opt;
+    btn.className = "answer-btn";
 
+    // show previous answer if exists
+    if(state.answers[state.current] === i){
+      btn.classList.add(i === q.answer ? "correct" : "wrong");
+    }
 
-document.getElementById(
+    btn.onclick = () => {
 
-"counter"
+      state.answers[state.current] = i;
 
-).innerText =
+      if(i === q.answer){
+        state.score++;
+      }
 
-(current+1)
+      saveState(state);
+      loadQuestion();
+    };
 
-+
+    box.appendChild(btn);
+  });
 
-" / "
-
-+
-
-questions.length;
-
-
-
-document.getElementById(
-
-"sign"
-
-).src = q.image;
-
-
-
-const box =
-
-document.getElementById(
-
-"answers"
-
-);
-
-
-box.innerHTML = "";
-
-
-
-q.options.forEach(
-
-(opt,i)=>{
-
-
-const btn=
-
-document.createElement(
-
-"button"
-
-);
-
-
-btn.className=
-
-"answer-btn";
-
-
-btn.innerText=
-
-opt;
-
-
-
-btn.onclick=()=>{
-
-
-if(
-
-i===q.answer
-
-){
-
-btn.classList.add(
-
-"correct"
-
-);
-
-
-score++;
-
-
+  updateProgress();
 }
 
-
-else{
-
-
-btn.classList.add(
-
-"wrong"
-
-);
-
-
+function updateProgress(){
+  const percent = ((state.current + 1) / questions.length) * 100;
+  document.getElementById("bar").style.width = percent + "%";
 }
 
-
+document.getElementById("next").onclick = () => {
+  if(state.current < questions.length - 1){
+    state.current++;
+    saveState(state);
+    loadQuestion();
+  }
 };
 
-
-box.appendChild(
-
-btn
-
-);
-
-
-});
-
-
-}
-
-
-
-document.getElementById(
-
-"next"
-
-).onclick=()=>{
-
-
-current++;
-
-
-if(
-
-current<questions.length
-
-){
-
-loadQuestion();
-
-}
-
-
-else{
-
-
-localStorage.setItem(
-
-"score",
-
-score
-
-);
-
-
-window.location=
-
-"result.html";
-
-
-}
-
-
+document.getElementById("prev").onclick = () => {
+  if(state.current > 0){
+    state.current--;
+    saveState(state);
+    loadQuestion();
+  }
 };
-
-
 
 loadQuestion();
