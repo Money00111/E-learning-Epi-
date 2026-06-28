69bd5115eb9b896d1f3bb97bfc6f4ce123bf541e
Money00111 <niyigenaepizo9@gmail.com>
2026-06-28 14:41:40 +0200

    Refactor getState function and add state management

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,22 @@
 const STORAGE_KEY = "epi_driving_state";
+const STORAGE_KEY = "epi_driving_state";
 
+function getState(){
+    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {
+        current:0,
+        score:0,
+        answers:{},
+        timeLeft:1800 // 30 minutes
+    };
+}
+
+function saveState(state){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+}
+
+function resetState(){
+    localStorage.removeItem(STORAGE_KEY);
+}
 function getState(){
   return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {
     current: 0,
